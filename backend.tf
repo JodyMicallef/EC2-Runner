24adf7e83b53ebc4aa6e3f19aa4be606333ff7df
JodyMicallef <jmicaallef@yahoo.co.uk>
2025-08-30 15:15:45 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = ****CHANGEBUCKETNAME***
+    bucket         = "ec2runnerk8"
     region         = "eu-west-2"
     key            = "Github-Runner-TF/terraform.tfstate"
     encrypt        = true
@@ -13,3 +13,4 @@ terraform {
     }
   }
 }
+
